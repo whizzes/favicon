9d3ab3f4fedd8b32da2c220f0c74d1b8934d2d31
Michael Liendo <michael.m.liendo.r@gmail.com>
2023-06-03 18:08:00 -0400

    test: `queue_many` test

--- src/favicon.rs
+++ src/favicon.rs
@@ -5,6 +5,7 @@ use std::str::FromStr;
 use anyhow::{Error, Result};
 use image::imageops::FilterType;
 use image::DynamicImage;
+use image::ImageOutputFormat;
 
 use crate::preset::Preset;
 
@@ -55,38 +56,18 @@ impl Favicon {
 
 #[cfg(test)]
 mod tests {
-    use std::path::PathBuf;
-
-    use image::ImageOutputFormat;
-
-    use crate::favicon::Favicon;
-    use crate::preset::Preset;
+    use super::*;
 
     #[test]
-    fn stores_presets_in_job_queue() {
+    fn test_queue_many() {
         let mut favicon = Favicon::new(PathBuf::from("fixtures/rust.png")).unwrap();
+        let presets = vec![
+            Preset::new_static("fixtures/rust.png", ImageOutputFormat::Png, 100, 100),
+            Preset::new_static("fixtures/rust.png", ImageOutputFormat::Png, 200, 200),
+            Preset::new_static("fixtures/rust.png", ImageOutputFormat::Png, 300, 300),
+        ];
+        favicon.queue_many(presets);
 
-        favicon.queue(Preset::new(
-            "favicon-16x16.png",
-            ImageOutputFormat::Png,
-            16,
-            16,
-        ));
-        favicon.queue(Preset::new(
-            "favicon-32x32.png",
-            ImageOutputFormat::Png,
-            32,
-            32,
-        ));
-
-        assert_eq!(favicon.job_queue.len(), 2);
-        assert_eq!(
-            favicon.job_queue[0],
-            Preset::new("favicon-16x16.png", ImageOutputFormat::Png, 16, 16,)
-        );
-        assert_eq!(
-            favicon.job_queue[1],
-            Preset::new("favicon-32x32.png", ImageOutputFormat::Png, 32, 32,)
-        );
+        assert_eq!(favicon.job_queue.len(), 3);
     }
 }
